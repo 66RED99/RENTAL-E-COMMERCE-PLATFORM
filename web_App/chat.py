d979bcd1b1c001ccd6c9550a2e2dcf81e3c431ae
LEON JOSEPH <ivanjoseph3030@gmail.com>
2024-04-02 18:46:15 +0530

    80%
    removed unnecessary files

--- web_App/chat.py
+++ web_App/chat.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = ""
+openai.api_key = "enteryourapikeyhere"
 st=input()
 completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": st}])
 print(completion.choices[0].message.content)
